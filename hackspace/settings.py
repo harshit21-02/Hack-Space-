615cc614a13d10d68f21b747ed59ea48aaaab9fc
Harshit <2000520310026@ietlucknow.ac.in>
2023-09-11 01:31:10 +0530

    allowed azure

--- hackspace/settings.py
+++ hackspace/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-ALLOWED_HOSTS = ['harshit20.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['harshit20.pythonanywhere.com','127.0.0.1','llm-hackathon-codered.azurewebsites.net']
