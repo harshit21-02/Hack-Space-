ebd1b825b90dca7232c7ee076113311b120b2f6d
Harshit Chaurasia <81517470+harshit21-02@users.noreply.github.com>
2023-09-11 11:53:56 +0530

    Add or update the Azure App Service build and deployment workflow config

--- hackspace/settings.py
+++ hackspace/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'HOST': os.environ.get('DBHOST'),
         'USER': os.environ.get('DBUSER'),
         'PASSWORD': os.environ.get('DBPASS'),
+        'PORT': os.environ.get('DBPORT'),
     }
 }
 
